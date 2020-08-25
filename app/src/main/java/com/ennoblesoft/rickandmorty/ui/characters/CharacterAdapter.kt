67f75afeffb129569690653da93f1516c92c0915
Md Arif <arifbd@list.ru>
2020-08-25 18:37:42 +0600

    item character layout binding

--- app/src/main/java/com/ennoblesoft/rickandmorty/ui/characters/CharacterAdapter.kt
+++ app/src/main/java/com/ennoblesoft/rickandmorty/ui/characters/CharacterAdapter.kt
@@ -1,12 +1,8 @@
 package com.ennoblesoft.rickandmorty.ui.characters
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.ennoblesoft.rickandmorty.data.entities.Character
 import com.ennoblesoft.rickandmorty.databinding.ItemCharacterBinding
 import com.ennoblesoft.rickandmorty.ui.listener.ItemClickListener
@@ -23,7 +19,7 @@ class CharacterAdapter(private val listener: ItemClickListener) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
         val binding = ItemCharacterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return CharacterViewHolder(binding, listener)
+        return CharacterViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -31,33 +27,10 @@ class CharacterAdapter(private val listener: ItemClickListener) :
     }
 
     override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
-        holder.bind(items[position])
+        val character = items[position]
+        holder.binding.item = character
+        holder.binding.listener = listener
     }
 
-    class CharacterViewHolder(
-        private val binding: ItemCharacterBinding,
-        private val listener: ItemClickListener
-    ) : RecyclerView.ViewHolder(binding.root),
-        View.OnClickListener {
-        private lateinit var character: Character
-
-        init {
-            binding.root.setOnClickListener(this)
-        }
-
-        @SuppressLint("SetTextI18n")
-        fun bind(item: Character) {
-            this.character = item
-            binding.tvName.text = item.name
-            binding.tvStatus.text = """${item.species} - ${item.status}"""
-            Glide.with(binding.root)
-                .load(item.image)
-                .transform(CircleCrop())
-                .into(binding.image)
-        }
-
-        override fun onClick(p0: View?) {
-            listener.onItemClicked(character.id)
-        }
-    }
+    class CharacterViewHolder(val binding: ItemCharacterBinding) : RecyclerView.ViewHolder(binding.root)
 }
